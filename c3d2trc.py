2ec0ab8e340841cc263a1b6cd5d1c068d774baad
David PAGNON <contact@david-pagnon.com>
2021-03-31 11:04:55 +0200

    Add files via upload
    argparse for better use of c3d2trc

--- c3d2trc.py
+++ c3d2trc.py
@@ -16,6 +16,7 @@
 ## INIT
 import c3d
 import numpy as np
+import argparse
 
 
 ## AUTHORSHIP INFORMATION
@@ -30,11 +31,17 @@ __status__ = "Production"
 
 
 ## FUNCTIONS
-def c3d2trc(c3d_path):
+def c3d2trc(*args):
     '''
     Convert c3d to trc
     /!\ Only point data are retrieved. Analog data (force plates, emg) and computed data (angles, powers, etc) will be lost
     '''
+    c3d_path = args[0]['input']
+    if args[0]['output']:
+        trc_path = args[0]['output']
+    else:
+        trc_path = c3d_path.replace('.c3d', '.trc')
+    
     # c3d header
     reader = c3d.Reader(open(c3d_path, 'rb'))
     items_header = str(reader.header).split('\n')
@@ -43,13 +50,12 @@ def c3d2trc(c3d_path):
     value_item = [item[1] for item in items_header_list]
     header_c3d = dict(zip(label_item, value_item))
     
+    # c3d data: reads 3D points (no analog data) and takes off computed data
     labels = reader.point_labels
     index_labels_markers = [i for i, s in enumerate(labels) if 'Angle' not in s and 'Power' not in s and 'Force' not in s and 'Moment' not in s and 'GRF' not in s]
     labels_markers = [labels[ind] for ind in index_labels_markers]
     
     # trc header
-    trc_path = c3d_path.replace('.c3d', '.trc')
-    
     header0_str = 'PathFileType\t4\t(X/Y/Z)\t' + trc_path
 
     header1 = {}
@@ -72,7 +78,7 @@ def c3d2trc(c3d_path):
     with open(trc_path, 'w') as trc_o:
         trc_o.write(header_trc+'\n')
     
-    # trc data (only point data, analog data will be lost)
+    # trc data
         index_data_markers = np.sort(np.concatenate([np.array(index_labels_markers)*3, np.array(index_labels_markers)*3+1, np.array(index_labels_markers)*3+2]))
         t0 = int(float(header_c3d['first_frame'])) / int(float(header_c3d['frame_rate']))
         tf = int(float(header_c3d['last_frame'])) / int(float(header_c3d['frame_rate']))
@@ -83,6 +89,10 @@ def c3d2trc(c3d_path):
             trc_line = '{i}\t{t}\t'.format(i=i, t=trc_time[n]) + '\t'.join(map(str,c3d_line_markers))
             trc_o.write(trc_line+'\n')
     
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required = True, help='c3d input file name')
+    parser.add_argument('-o', '--output', required=False, help='trc output file name')
+    args = vars(parser.parse_args())
     
-    
-    
+    c3d2trc(args)
