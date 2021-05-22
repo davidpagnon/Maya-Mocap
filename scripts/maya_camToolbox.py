760d8149ea00e6edfc2b839a270cf4744c6f84c5
David Pagnon <contact@david-pagnon.com>
2021-05-22 08:36:56 +0200

    conflict resolution

--- scripts/maya_camToolbox.py
+++ scripts/maya_camToolbox.py
@@ -148,7 +148,6 @@ def setCamsfromCal_callback(*args):
         cmds.setAttr("defaultResolution.width", W) 
         cmds.setAttr("defaultResolution.height", H) 
         cmds.select(cam)
-        cmds.rotate(180,0,0, objectSpace=1, relative=1)
 
     cmds.select(cams)
     cmds.group(n='cameras')
@@ -175,7 +174,7 @@ def saveCalfromCam_callback(*args):
     matrix = [[fp/binning_factor, 0., cu/binning_factor,], [0.0, fp/binning_factor, cv/binning_factor,], [0.0, 0.0, 1.0,],]
     # Distortion
     disto = cmds.getAttr(cams[0] + 'Shape1.aiRadialDistortion')
-    distortions = [-disto/4, 0. ,0. ,0.,] # Cuisine pour passer de la distorsion de maya (fisheye?) a celle de opencv (pinhole?)
+    distortions = [-disto/4., 0. ,0. ,0.,] # Cuisine pour passer de la distorsion de maya (fisheye?) a celle de opencv (pinhole?)
     # Rotation, Translation
     rotation, translation = [], []
     for c in range(len(cams)):
@@ -185,8 +184,12 @@ def saveCalfromCam_callback(*args):
         Rmat = Rmat . dot(np.array([[1,0,0],[0,-1,0],[0,0,-1]])) # rotx 180
         Tmat = np.array(Mmat).reshape(4,4)[:3,3]
         
-        rotation.append(cv2.Rodrigues(Rmat.T)[0] .flatten()  .tolist())
-        translation.append( (-Rmat.T . dot(Tmat) ) .flatten() .tolist() ) # * 1000) . tolist() )
+        # maya to opencv system coordinates
+        Rmat_cv = Rmat.T
+        Tmat_cv = -Rmat.T . dot(Tmat)
+        
+        rotation.append(cv2.Rodrigues(Rmat_cv)[0] .flatten() .tolist())
+        translation.append(Tmat_cv.tolist() )
 
     # Save calibration as .toml file
     cal_folder = cmds.fileDialog2(dialogStyle=2, cap="Select folder to save calibration", fm=3)[0]
